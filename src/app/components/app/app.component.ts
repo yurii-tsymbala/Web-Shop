f014a4a179b9fb0d73407505577045bdb025325d
Yurii <zumbala2013@gmail.com>
2024-03-01 14:52:20 +0200

    Configure Header & Footer

---
+++ src/app/components/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../footer/footer.component';
+import { HeaderComponent } from '../header/header.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [HeaderComponent, FooterComponent, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'webshop';
+}
