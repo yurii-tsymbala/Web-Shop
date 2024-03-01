cdf98a2b767c11932233f6d8ed538d5e3c9f80aa
Yurii <zumbala2013@gmail.com>
2024-03-01 18:10:34 +0200

    Configure ProductService

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@angular/core";
+import { Product } from "../models/Product";
+import { HttpClient } from "@angular/common/http";
+import { BehaviorSubject, Observable, tap } from "rxjs";
+
+@Injectable({
+    providedIn: "root",
+})
+export class ProductService {
+    private readonly API_LINK = "http://makeup-api.herokuapp.com/api/v1/products.json";
+    private products$ = new BehaviorSubject<Product[]>([]);
+    readonly updatedProducts$ = this.products$.asObservable();
+
+    constructor(private http: HttpClient) {}
+
+    getProducts(): Observable<Product[]> {
+        return this.http.get<Product[]>(this.API_LINK).pipe(
+            tap((value) => {
+                this.products$.next(value);
+            })
+        );
+    }
+}
