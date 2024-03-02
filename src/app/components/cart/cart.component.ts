609c2102e6fc3b69a0e606dbadc27bd6b8cecf7c
Yurii <zumbala2013@gmail.com>
2024-03-02 19:08:15 +0200

    Polish UI in Cart

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,14 +1,22 @@
-import { Input } from '@angular/core';
-import { Component } from '@angular/core';
-import { CartItem } from '../../models/CartItem';
+import { Input } from "@angular/core";
+import { Component } from "@angular/core";
+import { CartItem } from "../../models/CartItem";
 
 @Component({
-  selector: 'cart',
-  standalone: true,
-  imports: [],
-  templateUrl: './cart.component.html',
-  styleUrl: './cart.component.scss'
+    selector: "cart",
+    standalone: true,
+    imports: [],
+    templateUrl: "./cart.component.html",
+    styleUrl: "./cart.component.scss",
 })
 export class CartComponent {
-  @Input() cartItem!: CartItem;
+    @Input() cartItem!: CartItem;
+
+    get formattedPrice(): string {
+        return `${this.cartItem.product.price}${this.cartItem.product.price_sign} ${this.cartItem.product.currency}`;
+    }
+
+    get formattedUrl(): string {
+        return `http:${this.cartItem.product.api_featured_image}`;
+    }
 }
