5d5367af252fb4e5c98a7390983c5d9efdf65fce
Yurii <zumbala2013@gmail.com>
2024-03-03 01:29:35 +0200

    Add Footer to CartShopComponent

--- src/app/components/cart-shop/cart-shop.component.ts
+++ src/app/components/cart-shop/cart-shop.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'cart-shop',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './cart-shop.component.html',
   styleUrl: './cart-shop.component.scss'
 })
