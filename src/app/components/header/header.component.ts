2b9f74233b31780462bad7be53ffd76e5e6ce931
Yurii <zumbala2013@gmail.com>
2024-03-02 21:42:12 +0200

    Delete useless code

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,38 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { ProductService } from '../../services/product.service';
-import { Observable, map } from 'rxjs';
-import { CartItem } from '../../models/CartItem';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit } from "@angular/core";
+import { ProductService } from "../../services/product.service";
+import { Observable, map } from "rxjs";
+import { CartItem } from "../../models/CartItem";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'header',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+    selector: "header",
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: "./header.component.html",
+    styleUrl: "./header.component.scss",
 })
 export class HeaderComponent implements OnInit {
-  cartItems$!: Observable<CartItem[]>;
+    cartItems$!: Observable<CartItem[]>;
 
-  constructor(private productService: ProductService) {}
+    constructor(private productService: ProductService) {}
 
-  ngOnInit(): void {
-      // this.fetchProducts();
-      this.observeData();
-  }
+    ngOnInit(): void {
+        this.observeData();
+    }
 
-  get cartItemsCount$(): Observable<number> {
-    return this.productService.cartProducts$.pipe(
-        map((cartItems) =>
-            [...cartItems]
-                .map((cartItem) => cartItem.counter)
-                .reduce((acc, item) => { return acc += item }, 0)
-        )
-    );
-}
+    get cartItemsCount$(): Observable<number> {
+        return this.productService.cartProducts$.pipe(
+            map((cartItems) =>
+                [...cartItems]
+                    .map((cartItem) => cartItem.counter)
+                    .reduce((acc, item) => { return acc += item }, 0)
+            )
+        );
+    }
 
-  private observeData(): void {
-    this.cartItems$ = this.productService.cartProducts$;
-}
-
+    private observeData(): void {
+        this.cartItems$ = this.productService.cartProducts$;
+    }
 }
