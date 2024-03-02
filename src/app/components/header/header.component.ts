560025e7e80622b98704a3868e39cb175edf9443
Yurii <zumbala2013@gmail.com>
2024-03-02 21:40:42 +0200

    Add itemsCounters to Header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Observable, map } from 'rxjs';
+import { CartItem } from '../../models/CartItem';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  cartItems$!: Observable<CartItem[]>;
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+      // this.fetchProducts();
+      this.observeData();
+  }
+
+  get cartItemsCount$(): Observable<number> {
+    return this.productService.cartProducts$.pipe(
+        map((cartItems) =>
+            [...cartItems]
+                .map((cartItem) => cartItem.counter)
+                .reduce((acc, item) => { return acc += item }, 0)
+        )
+    );
+}
+
+  private observeData(): void {
+    this.cartItems$ = this.productService.cartProducts$;
+}
 
 }
