c0253e52ff02b0c6052c30704fd13f12b4fe33fc
Yurii <zumbala2013@gmail.com>
2024-03-03 00:24:23 +0200

    Configure routes & CartShopComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { AppComponent } from "./components/app/app.component";
+import { CartShopComponent } from "./components/cart-shop/cart-shop.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "home", component: AppComponent },
+    { path: "", redirectTo: "/home", pathMatch: "full" },
+    { path: "main", component: CartShopComponent }
+];
---
+++ src/app/components/cart-shop/cart-shop.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cart-shop',
+  standalone: true,
+  imports: [],
+  templateUrl: './cart-shop.component.html',
+  styleUrl: './cart-shop.component.scss'
+})
+export class CartShopComponent {
+
+}
